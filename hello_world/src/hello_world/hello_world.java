a3326f7129328a91c18f88004c48b0d13e15b9bd
Daniel Thomas <tfriesdan@gmail.com>
2019-10-04 13:52:39 -0700

    started email app

---
+++ hello_world/src/hello_world/hello_world.java
@@ -0,0 +1,14 @@
+package hello_world;
+
+public class hello_world {
+
+	public static void main(String[] args) {
+		// Prints text 4 times.
+		int i;
+		for(i=0;i<4;i++)
+		{
+		System.out.println("Hello World!");
+		}
+	}
+
+}
